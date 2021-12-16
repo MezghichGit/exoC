d5d0821939d4d2d37c657f8266c1ae5e10ef8169
moahmed amine mezghich <amine.mezghich@ensi-uma.tn>
2021-12-16 17:02:13 +0100

    iterator sur une liste

--- ExoVector/main.cpp
+++ ExoVector/main.cpp
@@ -1,8 +1,12 @@
 #include <iostream>
 #include <vector>
+#include <deque>
+#include <list>
+
 using namespace std;
 
-class Animal{
+class Animal
+{
 
 private:
     string nom;
@@ -13,17 +17,60 @@ public:
     void info();
 
 };
-void Animal::info(){
-cout <<"Nom : "<< nom <<" Espece : "<<espece<<endl;
+void Animal::info()
+{
+    cout <<"Nom : "<< nom <<" Espece : "<<espece<<endl;
 }
 
 Animal::Animal(string n, string e)
 {
-  nom=n;
-  espece = e;
+    nom=n;
+    espece = e;
 }
 int main()
 {
+    list<char> coll;
+
+    for (char c = 'a'; c<='z'; c++)
+        coll.push_back(c);
+
+// affichage de la liste à l’aide d’un itérateur
+    list<char>::const_iterator pos;
+
+    for(pos = coll.begin(); pos != coll.end(); pos++)
+        cout << *pos << ' ';
+
+
+
+    /*
+    int t[] = {1, 3, 1, 6, 4, 1, 5, 2, 1 };
+    list<int> li(t, t+7); /*li contient : 1, 3, 1, 6, 4, 1, 5, 2, 1*/
+    /*while(!li.empty())
+
+    {
+       cout<< li.front()<< endl;
+       li.pop_front();
+
+    }*/
+
+   // li.remove (1);
+
+    /*
+    deque<int> d;
+    for(int i=0; i<10; ++i)
+    {
+        d.push_front( i );
+    }
+    for(int i=90; i<100; ++i )
+    {
+        d.push_back( i );
+    }
+    for(int i=0; i<d.size(); ++i)
+    {
+        cout << d[i] << endl;
+    }*/
+
+    /*
     vector<int> iv;	// vector a initialement 0 elements.	location: 007E0EC0
     iv.push_back(42); // Ajoute 42 à la fin du vector	iv[0] = 42
     cout << "location: " << &iv[0] << endl;
@@ -51,7 +98,7 @@ int main()
     cout << "\n iv[1] = " << iv[1] << endl;
     cout << "\n iv[2] = " << iv[2] << endl;
     cout << "*k ="<< *k << endl;
-
+    */
     /*
     Animal a1("Mac","Chien");
     Animal a2("Pichou","Chat");
