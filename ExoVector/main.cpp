8afe5ca767f858b31ea073f3ebe8738765e3e54f
moahmed amine mezghich <amine.mezghich@ensi-uma.tn>
2021-12-16 16:30:04 +0100

    vector resize, reserve

--- ExoVector/main.cpp
+++ ExoVector/main.cpp
@@ -24,6 +24,35 @@ Animal::Animal(string n, string e)
 }
 int main()
 {
+    vector<int> iv;	// vector a initialement 0 elements.	location: 007E0EC0
+    iv.push_back(42); // Ajoute 42 à la fin du vector	iv[0] = 42
+    cout << "location: " << &iv[0] << endl;
+    int *k = &iv[0];
+    cout << "iv[0] = " << iv[0] << endl;
+    cout << "*k = " << *k << endl;
+    cout << "sz=" << iv.size() << " cap=" << iv.capacity();
+
+
+    cout << "\n************ reserve *************" << endl;
+
+    iv.reserve(1024); // augmente la capacité et non pas taille sz=2048 cap=2048
+    cout << "\niv[0]: " << iv[0] << endl;
+    cout << "\nlocation: " << &iv[0] << endl;
+    cout << "k = " << k << endl;
+    cout << "*k = " << *k << endl;
+    cout << "sz=" << iv.size() << " cap=" << iv.capacity();
+
+    cout << "\n************ resize *************" << endl;
+    iv.resize(2048); // augmente taille (et capacité)
+    cout << "\nlocation: " << &iv[0] << endl;
+    cout << "\n sz=" << iv.size()<< "cap="<< iv.capacity();
+
+    cout << "\n iv[0] = " << iv[0] << endl;
+    cout << "\n iv[1] = " << iv[1] << endl;
+    cout << "\n iv[2] = " << iv[2] << endl;
+    cout << "*k ="<< *k << endl;
+
+    /*
     Animal a1("Mac","Chien");
     Animal a2("Pichou","Chat");
     Animal a3("Toto","Oiseau");
@@ -37,7 +66,7 @@ int main()
     for(int i=0; i<va.size(); ++i )
     {
         va[i].info();
-    }
+    }*/
 
 
     /*
